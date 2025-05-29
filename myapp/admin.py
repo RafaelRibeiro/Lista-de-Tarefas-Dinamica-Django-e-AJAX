9893c98fce11269199308847e8692246b65e42b9
Rafael Ribeiro <rafael.ribeiro1705@gmail.com>
2025-05-29 18:34:20 -0300

    Comentando Admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
-from myapp.models import Status, TodoList
+from django.contrib import admin          # Importa as funcionalidades do Django Admin
+from myapp.models import Status, TodoList # Importa os modelos que você criou no models.py
 
-# Register your models here.
+# Registra o modelo Status no painel administrativo do Django.
+# Isso permite criar, editar, listar e excluir registros de Status pelo admin.
 admin.site.register(Status)
+
+# Registra o modelo TodoList no painel administrativo do Django.
+# Assim, tarefas (TodoList) podem ser gerenciadas diretamente pelo admin.
 admin.site.register(TodoList)
